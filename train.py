b733c83d2ecc0bd3a31791f3b84d375eef08d21f
Yoon-seop Lee <navy10021@snu.ac.kr>
2024-08-21 22:59:27 +0900

    Update train.py

--- train.py
+++ train.py
@@ -5,7 +5,10 @@ from fake_lense import *
 train_texts, test_texts, train_labels, test_labels = load_data("./data/train.csv", "./data/test.csv")
 
 # BERTLense : Train on BERT-based model
-bert_trainer, bert_lense, bert_tokenizer = train_bert('microsoft/deberta-base', train_texts, train_labels, test_texts, test_labels)
-
+bert_trainer, bert_lense, bert_tokenizer = train_bert(None, train_texts, train_labels, test_texts, test_labels, 1)
 # GPTLense : Train on GPT-based model 
-gpt_trainer, gpt_lense, gpt_tokenizer = train_gpt('EleutherAI/gpt-neo-125M', train_texts, test_texts)
+gpt_trainer, gpt_lense, gpt_tokenizer = train_gpt(None, train_texts, test_texts, 1)
+
+# If Continue training
+#gpt_trainer, gpt_lense, gpt_tokenizer = train_gpt(None, train_texts, test_texts, 1, True)
+#bert_trainer, bert_lense, bert_tokenizer = train_bert(None, train_texts, train_labels, test_texts, test_labels, 1, True)
