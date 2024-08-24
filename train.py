66399429e56a73ae4c9c7c656981df8a2f1d6948
Yoon-seop Lee <navy10021@snu.ac.kr>
2024-08-24 19:29:56 +0900

    Update train.py

--- train.py
+++ train.py
@@ -4,11 +4,11 @@ from fake_lense import *
 # Load Dataset
 train_texts, test_texts, train_labels, test_labels = load_data("./data/train.csv", "./data/test.csv")
 
-# BERTLense : Train on BERT-based model
+# BERTLense: Train on BERT-based model
 bert_trainer, bert_lense, bert_tokenizer = train_bert(None, train_texts, train_labels, test_texts, test_labels, 3)
-# GPTLense : Train on GPT-based model 
+# GPTLense: Train on GPT-based model 
 gpt_trainer, gpt_lense, gpt_tokenizer = train_gpt(None, train_texts, test_texts, 3)
 
-# Re-train : If user needs to continue training
+# Re-train: If the user needs to continue training
 #gpt_trainer, gpt_lense, gpt_tokenizer = train_gpt(None, train_texts, test_texts, 1, True)
 #bert_trainer, bert_lense, bert_tokenizer = train_bert(None, train_texts, train_labels, test_texts, test_labels, 1, True)
